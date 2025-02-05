7a900336025919c38af5f4d9af5dfc1d16030841
Konstantin Lapkov <k.lapkov@sap.com>
2025-02-05 20:57:25 +0200

    cc

--- app/controllers/runtime/apps_controller.rb
+++ app/controllers/runtime/apps_controller.rb
@@ -73,6 +73,7 @@ module VCAP::CloudController
                   running_env_json: EnvironmentVariableGroup.running.environment_json,
                   environment_json: process.app.environment_variables,
                   system_env_json: SystemEnvPresenter.new(process.service_bindings).system_env,
+                  system_env_json: SystemEnvPresenter.new(process).system_env,
                   application_env_json: { 'VCAP_APPLICATION' => vcap_application }
                 }, mode: :compat)
       ]
